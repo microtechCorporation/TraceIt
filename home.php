347e6edb93cff5277a8c97ecc4a0c750be5f11bf
Horacio Gune <gunehoracio@gmail.com>
2025-07-31 22:24:39 +0100

    criando a estrutura de diretorios

---
+++ home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TraceIt</title>
+</head>
+<body>
+    <h1>Welcome to TraceIt</h1>
+    <p>Your one-stop solution for all tracing needs.</p>
+</body>
+</html>
+    
